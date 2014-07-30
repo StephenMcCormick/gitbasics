04c7d82719fa1abadf7976d4f352eda4462ad725
Stephen McCormick <sjmccormick@email.neit.edu>
2014-07-29 21:21:07 -0400

    update hello.rb

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author Stephen McCormick (email)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
